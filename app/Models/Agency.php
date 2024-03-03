63e8ceed635decddd773c43ba4cf9758c3a1e6e4
Makabi Yelmis <gyelmis@gmail.com>
2024-03-03 07:46:50 +0430

    morning fix

--- app/Models/Agency.php
+++ app/Models/Agency.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Agency extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
 
     public function category(){
         return $this->belongsTo(Category::class);
