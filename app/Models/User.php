4f1f6790a1e11bab8946390932f28e5aa91a4ee2
Makabi Yelmis <gyelmis@gmail.com>
2024-04-09 15:30:55 +0430

    fix panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Filament\Tables\Columns\Layout\Panel;
+use Filament\Panel;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
